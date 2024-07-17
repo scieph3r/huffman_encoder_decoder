f11504db0900d907d2fd558f86076aa3eecefbec
scieph3r <raventheprogrammer@gmail.com>
2024-07-17 21:24:22 +0330

    add decompression functionality

--- hende/__main__.py
+++ hende/__main__.py
@@ -94,7 +94,8 @@ def main():
                 i += 8
     else:
         # read header and recreate character frequency mapping
-        char_freq_map = {}
+        char_freq_list = []
+        data = None
         with open(args.filepath, "rb") as fh:
             header_length = int.from_bytes(fh.read(2), byteorder="big")
             for _ in range(header_length):
@@ -111,8 +112,40 @@ def main():
                         pass
                     char_code += byte
                 freq = int.from_bytes(fh.read(4), byteorder="big")
-                char_freq_map[char_code.decode("utf-8")] = freq
+                char_freq_list.append((char_code.decode("utf-8"), freq))
+            
+            # read the data
+            data = fh.read()
+            data = str(bin(int.from_bytes(data[:], byteorder="big")))[2:]
+        # create Huffman tree
+        tree = create_tree(char_freq_list)
 
+        # create prefix code mapping
+        prefix_code_map = tree.get_prefix_codes()
+        code_to_char_map = {}
+        for key, val in prefix_code_map.items():
+            code_to_char_map[val] = key
+        
+        # garbage collection
+        prefix_code_map = None
+        
+        # get code lengths
+        lengths = list(set([len(x) for x in code_to_char_map.keys()]))
+        lengths.sort()
+
+        # translate
+        print("decompressing...")
+        i = 0
+        with open(args.output, "w") as fh:
+            while i < len(data):
+                for j in lengths:
+                    if code_to_char_map.get(data[i:i+j], None):
+                        fh.write(code_to_char_map[data[i:i+j]])
+                        i += j
+                        break
+                else:
+                    break
+                        
     return 0
 
 def create_tree(char_freq_lst):
